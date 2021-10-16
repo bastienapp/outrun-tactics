14c6a88bfd9282858d9ca04a7374e8709ef5cc81
PerrinGaetan <perringaetandu31@gmail.com>
2021-10-16 18:40:49 +0200

    add sructure to inject randomly sentence and possible answer of opponent

--- src/views/turn-view.php
+++ src/views/turn-view.php
@@ -1,8 +1,12 @@
 <?php 
 require_once __DIR__ . '/../models/opponent-model.php';
 require_once __DIR__ . '/../models/punchline-model.php';
-$sentencesOfAnOpponent = getPunchlineByOpponentId($_GET['opponent']);
 $opponents = getAllOpponent();
+$ranking = $_GET['position'];
+var_dump($ranking);
+$playerPos = strpos($ranking, "8");
+$opponentPos = $playerPos - 1;
+$sentencesOfAnOpponent = getRandomPunchlineByOpponentId($opponentPos);
 
 ?>
 
@@ -19,21 +23,21 @@ $opponents = getAllOpponent();
 <hr>
 
 <section class="section--quote">
-    <p class="section__p--quote"><?= getRandomPunchlineByOpponentId($_GET['opponent'])->sentence ?></p>
+    <p class="section__p--quote"><?= $sentencesOfAnOpponent->sentence ?></p>
 </section>
 
 <section class="section--quoteAndAnswer">
-    <form class="section__form--quoteAndAnswer" action="/traitement?lap=<?= $lap ?>&position=<?= $position ?>&answer= <?= $_GET['answer'] ?>" method="GET">
-        <label class="section__form__label--quoteAndAnswer" for="answer1"><?= getRandomPunchlineByOpponentId($_GET['opponent'])->answer_1 ?>
+    <form class="section__form--quoteAndAnswer" action="/traitement?lap=<?= $lap ?>&position=<?= $ranking ?>&answer= <?= $_GET['answer'] ?>" method="GET">
+        <label class="section__form__label--quoteAndAnswer" for="answer1"><?= $sentencesOfAnOpponent->answer_1 ?>
             <input type="radio" name="answer" id="answer1" value="answer1" class="section__div--quotesAndAnswers" />
         </label>
-        <label class="section__form__label--quoteAndAnswer" for="answer2"><?= getRandomPunchlineByOpponentId($_GET['opponent'])->answer_2 ?>
+        <label class="section__form__label--quoteAndAnswer" for="answer2"><?= $sentencesOfAnOpponent->answer_2 ?>
             <input type="radio" name="answer" id="answer2" value="answer2" class="section__div--quotesAndAnswers" />
         </label>
-        <label class="section__form__label--quoteAndAnswer" for="answer3"><?= getRandomPunchlineByOpponentId($_GET['opponent'])->answer_3 ?>
+        <label class="section__form__label--quoteAndAnswer" for="answer3"><?= $sentencesOfAnOpponent->answer_3 ?>
             <input type="radio" name="answer" id="answer3" value="answer3" class="section__div--quotesAndAnswers" />
         </label>
-        <label class="section__form__label--quoteAndAnswer" for="answer4"><?= getRandomPunchlineByOpponentId($_GET['opponent'])->answer_4 ?>
+        <label class="section__form__label--quoteAndAnswer" for="answer4"><?= $sentencesOfAnOpponent->answer_4 ?>
             <input type="radio" name="answer" id="answer4" value="answer4" class="section__div--quotesAndAnswers" />
         </label>
         <input class="section__form__label--submit" type="submit" value="Valider">
