db4a38e7a600b1d722e5a5caf8853e803f671db6
Asa <danielbatalhagoncalves.dev@gmail.com>
2021-10-16 17:36:15 +0200

    upgrade result page

--- src/routing.php
+++ src/routing.php
@@ -3,11 +3,11 @@
 $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ('/' === $urlPath) {
-    require __DIR__ . '/../public/index.php';
+    require __DIR__ . '/views/index.php';
 } elseif ('/turn' === $urlPath && isset($_GET['id'])) {
     require __DIR__ . '/views/turn-views.php';
 } elseif ('/result' === $urlPath) {
-    require __DIR__ . '/views/result-views.php';
+    require __DIR__ . '/views/result-view.php';
 } else {
     header('HTTP/1.1 404 Not Found');
 }
