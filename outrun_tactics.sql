2d42972e22ac13ce4e4b64bd845e1fa2e43c5baa
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-10-16 12:02:20 +0200

    ajout des personnages

--- outrun_tactics.sql
+++ outrun_tactics.sql
@@ -30,6 +30,14 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
+INSERT INTO `outrun_tactics`.`opponent` (`name`, `picture`, `description`) VALUES ('Sasuke D. Goku', '/images/opponents/sasuke-d-goku.jpg', 'Le courage et l\'amitié sont mes préceptes !');
+INSERT INTO `outrun_tactics`.`opponent` (`name`, `picture`, `description`) VALUES ('Crying 18', '/images/opponents/crying18.jpg', 'Tu n\'as pas idée de la pronfondeur du puit de souffrance qu\'est mon âme');
+INSERT INTO `outrun_tactics`.`opponent` (`name`, `picture`, `description`) VALUES ('Thibaut', '/images/opponents/thibaut.jpg', 'Défaite ? Je ne connais pas ce mot. Gagner, c\'est un état d\'esprit !');
+INSERT INTO `outrun_tactics`.`opponent` (`name`, `picture`, `description`) VALUES ('Ned de Speed', '/images/opponents/ned-de-speed.jpg', 'J\'ai calculé toutes les possibilités, je n\'en vois aucune où je perds');
+INSERT INTO `outrun_tactics`.`opponent` (`name`, `picture`, `description`) VALUES ('Santa Cruise', '/images/opponents/pere-noel.jpg', 'Oh oh oh, viens sur les genoux du Père Noël !');
+INSERT INTO `outrun_tactics`.`opponent` (`name`, `picture`, `description`) VALUES ('Tara Knight', '/images/opponents/tara-knight.jpg', 'Si tu ne veux pas disparaître étrangement, laisse-moi passer.');
+INSERT INTO `outrun_tactics`.`opponent` (`name`, `picture`, `description`) VALUES ('Lady Joana', '/images/opponents/lady-joana.jpg', 'Mon père m\'a acheté la meilleure voiture, mange ma poussière !');
+
 
 -- -----------------------------------------------------
 -- Table `outrun_tactics`.`punchline`
