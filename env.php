fe10d974866aa2319beaf37d68847579f900469a
Asa <danielbatalhagoncalves.dev@gmail.com>
2021-10-16 18:05:44 +0200

    add home to routing

--- env.php
+++ env.php
@@ -2,5 +2,5 @@
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'outrun_tactics');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'uujr24ar');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
--- src/routing.php
+++ src/routing.php
@@ -3,9 +3,9 @@
 $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ('/' === $urlPath) {
-    require __DIR__ . '/views/index.php';
-} elseif ('/turn' === $urlPath && isset($_GET['id'])) {
-    require __DIR__ . '/views/turn-views.php';
+    require __DIR__ . '/views/index-view.php';
+} elseif ('/turn' === $urlPath) {
+    require __DIR__ . '/views/turn-view.php';
 } elseif ('/result' === $urlPath) {
     require __DIR__ . '/views/result-view.php';
 } else {
