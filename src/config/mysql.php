b23a1c16e2a9fcaadb31f18020a1eff55310bd61
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-10-16 10:58:52 +0200

    add mysql config

--- src/config/mysql.php
+++ src/config/mysql.php
@@ -2,7 +2,6 @@
 
 require_once __DIR__ . '/../../env.php';
 
-// TODO PDO connection
 function getConnection(): PDO
 {
     return new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8');
