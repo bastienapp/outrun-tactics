43b68f936f41a153b7c1f60717f0a87b62ee3625
Asa <danielbatalhagoncalves.dev@gmail.com>
2021-10-17 10:07:51 +0200

    traitement of turn result

--- src/routing.php
+++ src/routing.php
@@ -6,7 +6,9 @@ if ('/' === $urlPath) {
     require __DIR__ . '/views/index-view.php';
 } elseif ('/turn' === $urlPath ) {
     require __DIR__ . '/views/turn-view.php';
-} elseif ('/result' === $urlPath) {
+} elseif ('/traitement' === $urlPath) {
+    require __DIR__ . '/views/traitement-view.php';
+}elseif ('/result' === $urlPath) {
     require __DIR__ . '/views/result-view.php';
 } else {
     header('HTTP/1.1 404 Not Found');
