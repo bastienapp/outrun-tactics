c0f868214bf7e3634183c801633db942bdf3e1ff
conradson <conradson+github@protonmail.com>
2021-10-17 11:53:34 +0200

    result css

--- src/views/result-view.php
+++ src/views/result-view.php
@@ -5,34 +5,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Result</title>
-    <link rel="stylesheet" href="style/result.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
+    <link rel="stylesheet" href="styles/result.css">
 </head>
 
 <body>
-<div class='result-container'>
-    <?php if (!empty($_GET)): ?>
-        <?php if ($_GET['result'] === 'victory'): ?>
-        <?php ?>
-            <h1 class='Titre'>
-                Victoire
-            </h1>
-            <div class='container-podium'>
-                <div class="first-bloc">2</div>
-                <div class="second-bloc">1</div>
-                <div class="third-bloc">3</div>
-            </div>
-            <input type="submit" value='Reset-Game'>
+    <?php
+        require_once __DIR__ . '/../models/opponent-model.php';
+        $position = $_GET['position'];
+        $podium = [];
+        for ($i = 0; $i < 3; $i++) {
+            $podium[] = getOpponentById(intval($position[$i]));        
+        }
+        $gameOver = strpos(substr($position, 0, 3), "8") === false;
+    ?>
+    <div class='result-container'>
+        <?php if ($gameOver): ?>
+        <h1>Game Over</h1>
         <?php else: ?>
-            <h1 class='Titre'>
-                Defaite
-            </h1>
-            <div class='container-Podium'>
-                <img src="image-podium.jpeg" alt="Podium">
-                <div class="container-Reset-Game">
-                    <input type="submit" value='Reset-Game'>
-                </div>
-            </div>
+        <h1>Bravo, vous êtes sur le podium !</h1>
         <?php endif; ?>
-    <?php endif; ?>
-</div>
+        <div class="podium podium__first">
+            <img src="<?= $podium[0]->picture ?>" alt="<?= $podium[0]->picture ?>" />
+            <h2>🥇 <?= $podium[0]->name ?></h2>
+            <h3><?= $podium[0]->description ?></h3>
+        </div>
+        <ul class="podium__other">
+            <li class="podium podium__second">
+                <img src="<?= $podium[1]->picture ?>" alt="<?= $podium[1]->picture ?>" />
+                <h3>🥈 <?= $podium[1]->name ?></h3>
+            </li>
+            <li class="podium podium__third">
+                <img src="<?= $podium[2]->picture ?>" alt="<?= $podium[2]->picture ?>" />
+                <h3>🥉 <?= $podium[2]->name ?></h3>
+            </li>
+        </ul>
+        <a href="/">Rejouer</a>
+    </div>
 </body>
