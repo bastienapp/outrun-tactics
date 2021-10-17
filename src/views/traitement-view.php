4a6cd4b20608c8fdf6102444645c3692eba60bf3
Asa <danielbatalhagoncalves.dev@gmail.com>
2021-10-17 10:57:53 +0200

    traitement ok

--- src/views/traitement-view.php
+++ src/views/traitement-view.php
@@ -5,30 +5,31 @@
  */
 
 
-$lap = $_GET['lap'];
+$lap = intval($_GET['lap']);
 $position = $_GET['position'];
 $answer = $_GET['answer'];
 
-
-
+$finalLap = 16;
 
 if ($answer === 'answer1') {
-    $indexOfEight = strrpos($position, "8");
-    $value = $position[$indexOfEight - 1];
-    $position[$indexOfEight - 1] = "8";
-    $position[$indexOfEight] = $value;
-    if (($position[0] === "8")){
-        header("Location: /turn?lap=" . $lap . "&position=" . $position);
+     $indexOfEight = strrpos($position, "8");
+     $value = $position[$indexOfEight - 1];
+     $position[$indexOfEight - 1] = "8";
+     $position[$indexOfEight] = $value;
 
-
-    }
-
-    header("Location: /turn?lap=" . $lap . "&position=" . $position);
-    die();
-} else {
-    header("Location: /turn?lap=" . $lap . "&position=" . $position);
-    die();
+     if (($position[0] == "8") || ($lap >= $finalLap)) {
+          header("Location: /result?lap=" . $lap . "&position=" . $position);
+          die();
+     } else {
+          header("Location: /turn?lap=" . $lap . "&position=" . $position);
+          die();
+     }
+     die();
+} elseif ($answer != 'answer1') {
+     if ($lap >= $finalLap) {
+          header("Location: /result?lap=" . $lap . "&position=" . $position);
+          die();
+     }
+     header("Location: /turn?lap=" . $lap . "&position=" . $position);
+     die();
 }
-
-
-
--- src/views/turn-view.php
+++ src/views/turn-view.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once __DIR__ . '/../models/opponent-model.php';
 require_once __DIR__ . '/../models/punchline-model.php';
 $lap = intval($_GET['lap']) + 1;
@@ -9,39 +9,37 @@ $playerPos = strpos($ranking, "8");
 $opponentPos = $playerPos - 1;
 $sentencesOfAnOpponent = getRandomPunchlineByOpponentId($ranking[$opponentPos]);
 $lapRanking = getOpponentsByPosition($ranking);
-$arrayIndex = [1,2,3,4]; 
+$arrayIndex = [1, 2, 3, 4];
 $randomIndex = shuffle($arrayIndex);
-var_dump($arrayIndex);
 ?>
 
 <link rel="stylesheet" href="styles/turn-view.css">
 <main>
 
 
-<h2>Classement actuel :</h2>
-<section class="section--ranking">
-    <?php foreach ($lapRanking as $rank) : ?>
-    <div class="section__div--ranking"><?= $rank['name'] ?></div>
-    <?php endforeach; ?>
-</section>
-<hr>
+     <h2>Classement actuel :</h2>
+     <section class="section--ranking">
+          <?php foreach ($lapRanking as $rank) : ?>
+               <div class="section__div--ranking"><?= $rank['name'] ?></div>
+          <?php endforeach; ?>
+     </section>
+     <hr>
 
-<section class="section--quote">
-    <p class="section__p--quote"><?= $sentencesOfAnOpponent->sentence ?></p>
-</section>
-<?php var_dump($sentencesOfAnOpponent) ?>
-<section class="section--quoteAndAnswer">
-    <form class="section__form--quoteAndAnswer" action="/traitement" method="GET">
-    <?php foreach ($arrayIndex as $index) : ?>
-        <?php $property = 'answer_' . $index; ?>
-        <label class="section__form__label--quoteAndAnswer" for="answer<?= $index ?>" ><?= $sentencesOfAnOpponent->$property ?>
-            <input type="radio" name="answer" id="answer<?= $index ?>" value="answer<?= $index ?>" class="section__div--quotesAndAnswers" />
-        </label>
-        <?php endforeach ?>
-        <input type="hidden" value="<? $ranking ?>" name="position" />
-        <input type="hidden" value="<? $lap ?>" name="lap" />
-        <input class="section__form__label--submit" type="submit" value="Valider">
-    </form>
-</section>
+     <section class="section--quote">
+          <p class="section__p--quote"><?= $sentencesOfAnOpponent->sentence ?></p>
+     </section>
+     <section class="section--quoteAndAnswer">
+          <form class="section__form--quoteAndAnswer" action="/traitement" method="GET">
+               <?php foreach ($arrayIndex as $index) : ?>
+                    <?php $property = 'answer_' . $index; ?>
+                    <label class="section__form__label--quoteAndAnswer" for="answer<?= $index ?>"><?= $sentencesOfAnOpponent->$property ?>
+                         <input type="radio" name="answer" id="answer<?= $index ?>" value="answer<?= $index ?>" class="section__div--quotesAndAnswers" />
+                    </label>
+               <?php endforeach ?>
+               <input type="hidden" value="<?= $ranking ?>" name="position" />
+               <input type="hidden" value="<? $lap ?>" name="lap" />
+               <input class="section__form__label--submit" type="submit" value="Valider">
+          </form>
+     </section>
 
 </main>
